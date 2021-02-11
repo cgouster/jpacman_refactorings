f7ad43e1ff6734c32a5abf3b4493dc5b9de1d2c0
chrisa1994 <chrisa.chrisa.g@gmail.com>
2021-02-11 16:17:33 +0200

    First Refactoring

---
+++ src/main/java/nl/tudelft/jpacman/level/PlayerCollisions.java
@@ -0,0 +1,67 @@
+package nl.tudelft.jpacman.level;
+
+import nl.tudelft.jpacman.board.Unit;
+import nl.tudelft.jpacman.npc.Ghost;
+
+/**
+ * A simple implementation of a collision map for the JPacman player.
+ * <p>
+ * It uses a number of instanceof checks to implement the multiple dispatch for the 
+ * collisionmap. For more realistic collision maps, this approach will not scale,
+ * and the recommended approach is to use a {@link CollisionInteractionMap}.
+ *
+ * @author Arie van Deursen, 2014
+ *
+ */
+
+public class PlayerCollisions implements CollisionMap {
+
+    @Override
+    public void collide(Unit mover, Unit collidedOn) {
+        mover.collide(collidedOn, this);
+    }
+
+    public void playerColliding(Player player, Unit collidedOn) {
+        if (collidedOn instanceof Ghost) {
+            playerVersusGhost(player, (Ghost) collidedOn);
+        }
+        if (collidedOn instanceof Pellet) {
+            playerVersusPellet(player, (Pellet) collidedOn);
+        }
+    }
+
+    public void ghostColliding(Ghost ghost, Unit collidedOn) {
+        if (collidedOn instanceof Player) {
+            playerVersusGhost((Player) collidedOn, ghost);
+        }
+    }
+
+    public void pelletColliding(Pellet pellet, Unit collidedOn) {
+        if (collidedOn instanceof Player) {
+            playerVersusPellet((Player) collidedOn, pellet);
+        }
+    }
+
+
+    /**
+     * Actual case of player bumping into ghost or vice versa.
+     *
+     * @param player The player involved in the collision.
+     * @param ghost The ghost involved in the collision.
+     */
+    public void playerVersusGhost(Player player, Ghost ghost) {
+        player.setAlive(false);
+    }
+
+    /**
+     * Actual case of player consuming a pellet.
+     *
+     * @param player The player involved in the collision.
+     * @param pellet The pellet involved in the collision.
+     */
+    public void playerVersusPellet(Player player, Pellet pellet) {
+        pellet.leaveSquare();
+        player.addPoints(pellet.getValue());
+    }
+
+}
---
+++ src/test/java/nl/tudelft/jpacman/board/BasicUnit.java
@@ -0,0 +1,28 @@
+package nl.tudelft.jpacman.board;
+
+import nl.tudelft.jpacman.level.PlayerCollisions;
+import nl.tudelft.jpacman.sprite.Sprite;
+
+/**
+ * Basic implementation of unit.
+ *
+ * @author Jeroen Roosen 
+ */
+class BasicUnit extends Unit {
+
+    /**
+     * Creates a new basic unit.
+     */
+    BasicUnit() {
+        super();
+    }
+
+    @Override
+    @SuppressWarnings("return.type.incompatible")
+    public Sprite getSprite() {
+        return null;
+    }
+
+	public void collide(Unit collidedOn, PlayerCollisions playerCollisions) {
+	}
+}
